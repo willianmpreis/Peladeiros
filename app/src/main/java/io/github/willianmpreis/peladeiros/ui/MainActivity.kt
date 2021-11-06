53b0883f2ca6cf17ff95a90935df7246314cb65b
Willian Reis <dev.wreis@gmail.com>
2021-11-06 14:57:48 -0300

    Estrutura inicial do projeto

--- app/src/main/java/io/github/willianmpreis/peladeiros/ui/MainActivity.kt
+++ app/src/main/java/io/github/willianmpreis/peladeiros/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.github.willianmpreis.peladeiros
+package io.github.willianmpreis.peladeiros.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.willianmpreis.peladeiros.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
